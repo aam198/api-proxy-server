04c4da9b76c8b21ddcbd3855a2440b106a10e2df
aam198 <aam198@psu.edu>
2022-01-26 10:28:17 -0500

    bug fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const rateLimit = require('express-rate-limit')
 require('dotenv').config()
-const serverless = require('serverless-http')
 
 const PORT = process.env.PORT || 5000
 
@@ -29,4 +28,3 @@ app.use(cors())
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 module.exports = app;
-module.exports.handler = serverless(app);
