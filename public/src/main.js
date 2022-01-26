17921d0b38769c273462c8eb76c51ce0aab5871b
aam198 <aam198@psu.edu>
2022-01-26 09:31:27 -0500

    DOM build

--- public/src/main.js
+++ public/src/main.js
@@ -1,8 +1,57 @@
 const weatherDisplay = document.querySelector('.weather');
-const weatherForm = document.querySelector('.weather-form');
+const weatherForm = document.querySelector('#weather-form');
 const cityInput = document.querySelector('#city-input');
 
-// Fetch weather data from API 
+// FetchAPI  weather data from OpenWeather API 
+const fetchWeather = async (city) => {
+  
+  const url= `/api?q=${city}`
+
+  const res = await fetch(url)
+  const data = await res.json()
+
+  if (data.cod === '404' ){
+    alert('City not found')
+    return
+  }
+
+  if (data.cod === 401) {
+    alert ('Invalid API Key')
+    return
+  }
+
+ // Check out the object in console
+ console.log(data);
+
+ // destructing the object
+ const displayData = {
+   city: data.name,
+   temp: kelvinToFahrenheit(data.main.temp),
+   feelLike: kelvinToFahrenheit(data.main.feels_like),
+   humidity: data.main.humidity
+ }
+
+ // Send gathered information to DOM
+ addWeatherToDOM(displayData)
+}
+
+// To Convert Kelvin to Fahrenheit
+
+const kelvinToFahrenheit = (temp) => {
+  return Math.ceil(((temp - 273.15) *9) / 5 +32)
+}
+
+// To add display data to DOM
+const addWeatherToDOM = (data) => {
+  weatherDisplay.innerHTML = `
+  <h1>Current Weather in ${data.city}</h1>
+  <h2>${data.temp} &deg;F</h2>
+  <h2>Real feel of ${data.feelLike} &deg;F</h2>
+  <h2>Humidity: ${data.humidity} %</h2>`
+
+  // Clear out input field
+  cityInput.value = ''
+}
 
 
 // Event listener for form submission - city input
